4f1ce66c63a071f753e249d7557b81887f227c57
mdkdinesh2503 <mdkdinesh2503@gmail.com>
2023-07-31 15:33:30 +0530

    Responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  isMenuOpen :boolean = false;
+  hide :boolean = true;
+
+  openMenu() {
+    this.isMenuOpen = true;
+    this.hide = false;
+  }
+  closeMenu() {
+    this.isMenuOpen = false;
+    this.hide = true;
   }
 
 }
